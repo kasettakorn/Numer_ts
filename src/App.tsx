4e9199a0364f8905487509da5209d334b1482597
Korn_SAMART <kasettakorn1997@gmail.com>
2022-03-02 17:27:18 +0700

    Initial

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+export default function App() {
+    return (
+        <div className="App">
+            <div className="area"></div><nav className="sidebar">
+                <ul>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-home fa-2x"></i>
+                            <span className="nav-text">
+                                Dashboard
+                            </span>
+                        </a>
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-laptop fa-2x"></i>
+                            <span className="nav-text">
+                                Stars Components
+                            </span>
+                        </a>
+
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-list fa-2x"></i>
+                            <span className="nav-text">
+                                Forms
+                            </span>
+                        </a>
+
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-folder-open fa-2x"></i>
+                            <span className="nav-text">
+                                Pages
+                            </span>
+                        </a>
+
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-bar-chart-o fa-2x"></i>
+                            <span className="nav-text">
+                                Graphs and Statistics
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-font fa-2x"></i>
+                            <span className="nav-text">
+                                Quotes
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-table fa-2x"></i>
+                            <span className="nav-text">
+                                Tables
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-map-marker fa-2x"></i>
+                            <span className="nav-text">
+                                Maps
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-info fa-2x"></i>
+                            <span className="nav-text">
+                                Documentation
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+
+                <ul className="logout">
+                    <li>
+                        <a href="#">
+                            <i className="fa fa-power-off fa-2x"></i>
+                            <span className="nav-text">
+                                Logout
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+
+        </div>
+    )
 }
 
-export default App;
